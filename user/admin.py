01ceb5aa6f43ebcff77c7713ba778adf357f1055
rostislavSLA <87258530+rostislavSLA@users.noreply.github.com>
2022-07-18 20:46:52 +0300

    admin user

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Urls
+from .models import User, Urls
 
 
 class UrlsAdmin(admin.ModelAdmin):
@@ -8,4 +8,4 @@ class UrlsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Urls, UrlsAdmin)
-
+admin.site.register(User)
